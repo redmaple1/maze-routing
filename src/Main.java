1ac976fa98cef0785fbc8406305b90c8458de1ef
renxiaoya <renxiaoya@zhihu.com>
2018-09-26 18:25:10 +0800

    创建迷宫数据模型

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: renxiaoya
+ * @Date: 2018/9/26 18 12
+ */
+public class Main {
+    public static void main(String[] args) {
+        String filename = "maze_101_101.txt";
+        MazeData data = new MazeData(filename);
+        data.print();
+    }
+}
